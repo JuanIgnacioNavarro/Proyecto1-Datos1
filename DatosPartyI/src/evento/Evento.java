b726008099554a0731eb0d6d326feda3c2e0b552
marti <marti@DESKTOP-V21NITN>
2020-05-19 02:10:07 -0600

    #5 Se agregan las clases previstas en el diagrama de clases

---
+++ DatosPartyI/src/evento/Evento.java
@@ -0,0 +1,5 @@
+package evento;
+
+public enum Evento {
+
+}
---
+++ DatosPartyI/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import juego.Bienvenida;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Bienvenida bienvenida = new Bienvenida();
+
+	}
+
+}
