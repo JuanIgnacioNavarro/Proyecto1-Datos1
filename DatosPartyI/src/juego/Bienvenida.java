70dae7d6ce893902776e91e62e57ddd7f4c952bb
Andrés Martínez Vargas <martinezandres@estudiantec.cr>
2020-05-21 15:01:04 -0600

    #13 se corrige el commit

--- DatosPartyI/src/juego/Bienvenida.java
+++ DatosPartyI/src/juego/Bienvenida.java
@@ -4,22 +4,48 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Bienvenida {
-	JButton b1;
-	JLabel l1;
-	public Bienvenida()
-	{
-		setTitle("Datos Party");
+public class Bienvenida extends JFrame {
+	public static Color colorVentana = new Color(94, 94, 94);
+	private JPanel panelBienvenida;
+	private JButton b1;
+	private JLabel l1;
+	public Bienvenida() {
+		setTitle("Bienvenido a Datos Party I");
 		setSize(646,700);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 
 		setLayout(new BorderLayout());
-		setContentPane(new JLabel(new ImageIcon("DatosPartyI/Imagenes/Fondo.jpg")));
+		setContentPane(new JLabel(new ImageIcon("Imagenes/Fondo.jpg")));
 		setLayout(new FlowLayout());
-		l1=new JLabel("Here is a button");
-		b1=new JButton("I am a button");
-		add(l1);
-		add(b1);
+		
+		agregarPanel();
+		agregarEtiqueta();
+		agregarBoton();
+
+	}
+	
+	private void agregarPanel() {
+		panelBienvenida = new JPanel();
+		this.getContentPane().add(panelBienvenida);
+//		panelBienvenida.setLayout(null);
+		panelBienvenida.setBackground(colorVentana);
+		
+	}
+	private void agregarEtiqueta() {
+		l1 = new JLabel("Here is a button");
+		panelBienvenida.add(l1);
+		
+	}
+	private void agregarBoton() {
+		b1 = new JButton("I am a button");
+		panelBienvenida.add(b1);
+
+	}
+	
+	public static void main(String[] args){
+		Bienvenida b = new Bienvenida();
+	}
+	
 }
