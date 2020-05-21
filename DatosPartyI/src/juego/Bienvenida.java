6d6b76fad096e8f466c4062bd1f07820c48803de
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-05-21 11:40:50 -0600

    #7 Ventana de inicio
    La ventana de inicio puede ser instanciada en el main

--- DatosPartyI/src/juego/Bienvenida.java
+++ DatosPartyI/src/juego/Bienvenida.java
@@ -1,9 +1,25 @@
 package juego;
 
-import java.awt.Color;
-import java.awt.Font;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Bienvenida {
-	public static Color colorVentana = new Color(94, 94, 94);
+	JButton b1;
+	JLabel l1;
+	public Bienvenida()
+	{
+		setTitle("Datos Party");
+		setSize(646,700);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 
+		setLayout(new BorderLayout());
+		setContentPane(new JLabel(new ImageIcon("DatosPartyI/Imagenes/Fondo.jpg")));
+		setLayout(new FlowLayout());
+		l1=new JLabel("Here is a button");
+		b1=new JButton("I am a button");
+		add(l1);
+		add(b1);
 }
